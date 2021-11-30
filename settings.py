2ee913c46edf80fe283604563dc26534e45baf0d
luan nguyen <luan.dataminer@gmail.com>
2021-12-01 01:21:14 +0700

    updated

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
-# Your username's Ahrefs 
+# Your username's Ahrefs
 USERNAME = "gad.alon@gmail.com"
 
-# Your password's Ahrefs 
+# Your password's Ahrefs
 PASSWORD = "KJwf43$sd24"
 
 # Show the web browser when running script
@@ -9,16 +9,16 @@ PASSWORD = "KJwf43$sd24"
 BROWSER_IN_BACKGROUND = True
 
 # Search backlink
-SEARCH_BACKLINK_PROTOCOL = None # None: both | http | https
-SEARCH_BACKLINK_INDEX_MODE = 'e' # e: exact | p: prefix | d: domain | None: subdomain
+SEARCH_BACKLINK_PROTOCOL = None  # None: both | http | https
+SEARCH_BACKLINK_INDEX_MODE = "e"  # e: exact | p: prefix | d: domain | None: subdomain
 
 # Input & output File
 # File name will store in data/[your file name] and file name with extention is xlsx
-INPUT_FILE_NAME = 'input.xlsx' 
-OUTPUT_1_FILE_NAME = 'output-1.xlsx'
-OUTPUT_2_FILE_NAME = 'output-2.xlsx'
-OUTPUT_3_FILE_NAME = 'output-3.xlsx'
-OUTPUT_4_FILE_NAME = 'output-4.xlsx'
+INPUT_FILE_NAME = "input.xlsx"
+OUTPUT_1_FILE_NAME = "output-1.xlsx"
+OUTPUT_2_FILE_NAME = "output-2.xlsx"
+OUTPUT_3_FILE_NAME = "output-3.xlsx"
+OUTPUT_4_FILE_NAME = "output-4.xlsx"
 
 # Your Blacklist keyword file. Need file with extention is .txt
-BACKLIST_KEYWORDS_FILE = 'blacklist keywords.txt'
+BACKLIST_KEYWORDS_FILE = "blacklist keywords.txt"
